d7451275ae0f2b6849a89026b8f91b930dd4bf04
Nils Leo <nilstleo01@gmail.com>
2023-07-21 00:07:33 +0200

    chore(pom.xml): remove unused quarkus-resteasy dependency to optimize build feat(pom.xml): add dependencies for quarkus-smallrye-jwt-build and java-jwt to enable JWT token validation refactor(TokenValidator.java): replace GoogleIdTokenVerifier with JWT.decode to validate JWT token and extract user information

--- remsfal-service/src/main/java/de/remsfal/service/boundary/authentication/TokenValidator.java
+++ remsfal-service/src/main/java/de/remsfal/service/boundary/authentication/TokenValidator.java
@@ -1,49 +1,29 @@
 package de.remsfal.service.boundary.authentication;
-
-import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
-import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
-import com.google.api.client.http.javanet.NetHttpTransport;
-import com.google.api.client.json.gson.GsonFactory;
 import de.remsfal.core.dto.ImmutableUserJson;
 import de.remsfal.core.model.UserModel;
-
+import io.smallrye.jwt.build.Jwt;
 import javax.enterprise.context.ApplicationScoped;
-import java.io.IOException;
-import java.security.GeneralSecurityException;
-import java.util.Collections;
+import java.util.*;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.interfaces.DecodedJWT;
 
 @ApplicationScoped
 public class TokenValidator {
-    private static final String CLIENT_ID = "821093255871-m7fg18oh8je55vaknjur9pgrh8sh4atb.apps.googleusercontent.com";
-
     public TokenInfo validate(final String authorizationHeader) {
-        String idTokenString = authorizationHeader.replace("Bearer ", "");
+        String token = authorizationHeader.replace("Bearer ", "");
+        DecodedJWT jwt = JWT.decode(token);
+        String email = jwt.getClaim("email").asString();
+        String userId = jwt.getSubject(); // "sub" is generally the subject in a JWT
+        String name = jwt.getClaim("name").asString();
 
-        GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(new NetHttpTransport(), new GsonFactory())
-                .setAudience(Collections.singletonList(CLIENT_ID))
+        final UserModel user = ImmutableUserJson.builder()
+                .id(userId)
+                .email(email)
+                .name(name)
                 .build();
 
-        try {
-            GoogleIdToken idToken = verifier.verify(idTokenString);
-            if (idToken != null) {
-                GoogleIdToken.Payload payload = idToken.getPayload();
-
-                String userId = payload.getSubject();
-                String email = payload.getEmail();
-                String name = (String) payload.get("name");
-            System.out.println("payload " + payload);
-                final UserModel user = ImmutableUserJson.builder()
-                        .id(userId)
-                        .email(email)
-                        .name(name)
-                        .build();
-
-                return new TokenInfo(user);
-            }
-        } catch (GeneralSecurityException | IOException e) {
-            System.out.println("Invalid ID token.");
-        }
-
-        return null;
+        return new TokenInfo(user);
     }
+
+
 }
